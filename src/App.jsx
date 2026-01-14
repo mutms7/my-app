48840edf774dd8346374b043d60d7db0c3af5224
William Chenyin <welcomehomemutms7@gmail.com>
2026-01-13 22:21:03 -0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 
 
 export const App = () => {
-  return ( <h1>hello this is a good project</h1>);
+  const username = "William"
+
+  const greeting = (name) => `Hello, ${name}`;
+  const userinfo = { age: 18, location: "Waterloo"}
+
+  return (
+  <>
+  <h1>This is a good project</h1>
+  <p>Your name is: {username}</p>
+  <p>Greeting: {greeting(username)}</p>
+  <p>
+    Age: {userinfo.age}, Location: {userinfo.location}
+  </p>
+  </>)
 }
  
 //export default App;
